d0f9162e197b11ca9ac26f1be97f783aea7624a5
Joham Romucho <joham.romucho@pucp.edu.pe>
2023-06-04 00:04:18 -0500

    feat: set up eureka server

---
+++ src/test/java/com/example/dseurekaserver/DsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
